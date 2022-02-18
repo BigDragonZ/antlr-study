de689483c85f9405300331520d9ea631cc13c5f4
BigdragonZ <291640894@qq.com>
2022-02-18 10:25:24 +0800

    Initial commit

---
+++ src/main/java/cloud/bigdragon/antlr/arrayinit/test/ShortToUnicodeString.java
@@ -0,0 +1,27 @@
+package cloud.bigdragon.antlr.arrayinit.test;
+
+import cloud.bigdragon.antlr.arrayinit.source.ArrayInitBaseListener;
+import cloud.bigdragon.antlr.arrayinit.source.ArrayInitParser;
+
+// 将类似{1,2,3}的short数组初始化语句翻译为"\u0001\u0002\u0003"
+public class ShortToUnicodeString extends ArrayInitBaseListener {
+    //    将 { 翻译为 "
+    @Override
+    public void enterInit(ArrayInitParser.InitContext ctx) {
+        System.out.println('"');
+    }
+
+    //    将 } 翻译为 "
+    @Override
+    public void exitInit(ArrayInitParser.InitContext ctx) {
+        System.out.println('"');
+    }
+
+//    将整数翻译为十六进制 加前缀\u
+    @Override
+    public void enterValue(ArrayInitParser.ValueContext ctx) {
+//        此处假设不存在嵌套结构
+        int value = Integer.valueOf(ctx.INT().getText());
+        System.out.printf("\\u%04x", value);
+    }
+}
